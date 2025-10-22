7a09c360febe76263ca6b3901c07914cb360f2b9
Rakshak-ai <rakshakaisos@gmail.com>
2025-10-23 04:11:49 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,18 @@
+const CACHE_NAME = "rakshakai-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./assets/logo.png",
+  "./assets/sos-icon.png",
+  "./assets/chatbot-icon.png"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
