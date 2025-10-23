149d6af3849783ae8f6314b0629a2a2f86ba9aa1
Rakshak-ai <rakshakaisos@gmail.com>
2025-10-23 23:03:32 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCsRQkWC6-Apwt4_ynoeds8tHIdOgMTRpg",
   authDomain: "rakshakai.firebaseapp.com",
@@ -19,3 +19,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Firestore, Auth, and Storage
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { db, auth, storage };
